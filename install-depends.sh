da4e0d93979bd375eb53ae206e1721e6a72bb2f0
Samuel Lord <sllord@icloud.com>
2018-10-03 11:51:43 -0500

    Create install-depends.sh

---
+++ install-depends.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo Installing... this may take awhile!
+sudo apt install -y winetricks
+winetricks wininet winhttp vcrun2008 pdh
+wget https://github.com/git-for-windows/git/releases/download/v2.19.0.windows.1/Git-2.19.0-32-bit.exe
+chmod +x "Git-2.19.0-32-bit.exe"
+wine "Git-2.19.0-32-bit.exe"
+wget https://dl.google.com/go/go1.10.4.windows-386.msi
+chmod +x "go1.10.4.windows-386.msi"
+wine "go1.10.4.windows-386.msi"
+echo Done.
