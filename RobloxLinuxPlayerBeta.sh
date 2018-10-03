23912bdd07866df4a98ac14fa72112dbca49a66f
Samuel Lord <sllord@icloud.com>
2018-10-03 11:34:38 -0500

    Update RobloxLinuxPlayerBeta.sh

--- RobloxLinuxPlayerBeta.sh
+++ RobloxLinuxPlayerBeta.sh
@@ -5,9 +5,6 @@ winetricks wininet winhttp vcrun2008 pdh
 wget http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.1/win32/en-US/Firefox%20Setup%2017.0.1.exe
 chmod +x "Firefox Setup 17.0.1.exe"
 wine "Firefox Setup 17.0.1.exe"
-wget http://setup.rbxcdn.com/version-6c991273eb8a46b2-Roblox.exe
-chmod +x "version-6c991273eb8a46b2-Roblox.exe"
-wine "version-6c991273eb8a46b2-Roblox.exe"
 echo Done. 
 echo Make sure that, when you open Firefox for the first time,
 echo that you disable updates!
