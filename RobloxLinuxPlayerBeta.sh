43433fec2a9c55feac2c3206fd0097176baf76f3
Samuel Lord <sllord@icloud.com>
2018-10-03 11:56:50 -0500

    Update RobloxLinuxPlayerBeta.sh

--- RobloxLinuxPlayerBeta.sh
+++ RobloxLinuxPlayerBeta.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-echo Installing... this may take awhile!
-sudo apt install -y winetricks
-winetricks wininet winhttp vcrun2008 pdh
-wget http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/17.0.1/win32/en-US/Firefox%20Setup%2017.0.1.exe
-chmod +x "Firefox Setup 17.0.1.exe"
-wine "Firefox Setup 17.0.1.exe"
-echo Done. 
-echo Make sure that, when you open Firefox for the first time,
-echo that you disable updates!
+echo Place ID:
+read placeid
+echo Username:
+read usrname
+wine rbxlaunch -id $placeid -u $usrname
