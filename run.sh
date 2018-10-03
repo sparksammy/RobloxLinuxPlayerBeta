7a0fdf98988e2489fe5baebb8e482a00172daa5b
Samuel Lord <sllord@icloud.com>
2018-10-03 13:01:19 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+wget https://raw.githubusercontent.com/Anaminus/rbxlaunch/master/launch.go
 echo Place ID:
 read placeid
 echo Username:
 read usrname
-wine rbxlaunch -id $placeid -u $usrname
+wine go launch.go -id $placeid -u $usrname
